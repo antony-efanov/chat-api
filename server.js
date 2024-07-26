a6c1b7577f1642857f2e6909871f3e7187461254
Anton Yefanov <xt.yefonova@gmail.com>
2024-07-26 14:41:23 +0300

    updated cors

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.get('/', (req, res) => {
 const server = http.createServer(app);
 const io = socketIo(server, {
   cors: {
-    origin: "http://localhost:3001",
+    origin: ["http://localhost:3001", "https://chat-client-virid.vercel.app"],
     methods: ["GET", "POST"],
   },
 });
