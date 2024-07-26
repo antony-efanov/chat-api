9871a134e558b36782d4600ddb38cac71b0fae55
Anton Yefanov <xt.yefonova@gmail.com>
2024-07-26 14:32:29 +0300

    changed server.js to be server

--- server.js
+++ server.js
@@ -1,4 +1,16 @@
-const io = require("socket.io")(3000, {
+// server.js
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+const app = express();
+const PORT = 4000;
+
+app.get('/home', (req, res) => {
+  res.status(200).json('Welcome, your app is working well');
+});
+
+const server = http.createServer(app);
+const io = socketIo(server, {
   cors: {
     origin: "http://localhost:3001",
     methods: ["GET", "POST"],
@@ -16,3 +28,10 @@ io.on("connection", (socket) => {
     console.log("A user is disconnected");
   });
 });
+
+server.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
+
+// Export the Express API
+module.exports = app;
