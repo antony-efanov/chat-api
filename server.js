db3ab314fec97a774985c50f7f96a0bd0ca65235
Anton Yefanov <xt.yefonova@gmail.com>
2024-07-26 14:46:46 +0300

    updated cors 2

--- server.js
+++ server.js
@@ -2,8 +2,15 @@
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
+const cors = require('cors');
 const app = express();
 const PORT = 4000;
+
+app.use(cors({
+  origin: ["http://localhost:3001", "https://chat-client-virid.vercel.app"],
+  methods: ["GET", "POST"],
+  credentials: true
+}));
 
 app.get('/', (req, res) => {
   res.status(200).json('Welcome, your app is working well');
@@ -14,6 +21,7 @@ const io = socketIo(server, {
   cors: {
     origin: ["http://localhost:3001", "https://chat-client-virid.vercel.app"],
     methods: ["GET", "POST"],
+    path: '/socket.io'
   },
 });
 
